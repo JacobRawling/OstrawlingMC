f63e56208038b6ee001bb63d62e04b862d7a30f3
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 21:03:33 +0000

    Added event number saving

--- ostrawling/saver/csv_saver.py
+++ ostrawling/saver/csv_saver.py
@@ -15,16 +15,16 @@ class CSVSaver(Saver):
     def close_file(self):
         self.output_file.close()
 
-    def save_event(self, event):
+    def save_event(self, event, event_number):
 
         # Read particle by particle
-        event_line = ""
+        event_line = "%d, "%event_number
         for p in event.final_state_particles:
             event_line += "%d, %.4g, %.4g, %.4g, %.4g,"%(p.pdg_id, p.momentum.px(),p.momentum.py(), p.momentum.pz(), p.momentum.m())
 
         # remove the trailing comma
-        event_line = event_line[:-1]
-        f.write(event_line)
+        event_line = event_line[:-1] + '\n'
+        self.output_file.write(event_line)
 
 
 
