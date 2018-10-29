e0ff33b1af09521b1970577960f198eb588ce7ad
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 20:05:44 +0000

    Removed typos where member variables missed self.

--- ostrawling/generator.py
+++ ostrawling/generator.py
@@ -3,35 +3,36 @@ Authors: Jacob Rawling & Kiran Ostrolenk
 
 @Brief: General class to manage MC generation of events 
 """
+from .saver import create_saver
+import logging 
 
 class Generator:
-    def __init__(self):
-        self.processess = None
-        self.saver = None
-        pass
-
+    def __init__(self, process, saver='csv', config=None):
+        self.process = process
+        self.saver = create_saver(saver)
+        self.config = config
 
     def generate_n_event(self, n):
-        events = []
+        events, total_integral = [], 0.
         for i in range(n):
             # Sample a random event that obeys conservation laws
-            ps_point = process.draw_phasespace_point() 
+            ps_point = self.process.draw_phasespace_point() 
 
             # Integrate the 
-            integral = process.integrand(ps_point) 
+            integral = self.process.integrand(ps_point) 
             total_integral += integral 
 
             # Store all events
-            events.append( process.phase_space_to_event(ps_point) )
+            events.append(self.process.phase_space_to_event(ps_point) )
             events[-1].set_weight(integral)
 
         self.events = events
-        self.xsec = process.get_volume() * total_integral/n
+        self.xsec = self.process.volume() * total_integral/n
         return self.xsec
 
     def save(self):
-
-        
-        for e in self.events:   
-            self.saver.new_event(e)
+        pass
+        # print(self.events)
+        # for e in self.events:   
+        #     self.saver.new_event(e)
 
