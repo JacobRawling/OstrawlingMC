1d8e1b11cffa8b1c15f51eb4f84a7863bf6d6959
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 16:36:42 +0000

    Added event dummy classes and updated module to know about these

--- ostrawling/__init__.py
+++ ostrawling/__init__.py
@@ -1,4 +1,4 @@
 from .generator import Generator
 from .processes import *
 from .saver import *
-from .solver import *
+from .event import *
---
+++ ostrawling/event/__init__.py
@@ -0,0 +1,3 @@
+from .event import *
+from .particle import *
+from .four_momenta import *
