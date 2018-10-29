eca2f99d44cc0c88806787df11cb586b91c1aa19
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 20:06:07 +0000

    Updated tests to anticipate new way of initializing four-momenta.

--- tests/run_all_unit_tests.py
+++ tests/run_all_unit_tests.py
@@ -4,21 +4,17 @@ import numpy as np
 
 class TestParticleMethods(unittest.TestCase):
     def test_el_mass(self):
-        el = ost.Particle(pdg_id=11, momentum=None) 
-        self.assertEqual( el.mass, 0.5e-3)
+        el = ost.Particle(pdg_id=11, momentum=ost.FourMomentum((1.,1.,1.,1.))) 
+        self.assertEqual( el.mass(), 0.5e-3)
 
     def test_mu_mass(self):
-        mu = ost.Particle(pdg_id=13, momentum=None) 
-        self.assertEqual(mu.mass, 106e-3)
+        mu = ost.Particle(pdg_id=13, momentum=ost.FourMomentum((1.,1.,1.,1.))) 
+        self.assertEqual(mu.mass(), 106e-3)
 
 class TestFourMomentumMethods(unittest.TestCase):
-    def test_p4_mass(self):
-        with self.assertRaises( ValueError):
-            ost.FourMomentum(px=1,py=1,pz=1,m=1,e=1)
-
     def test_p4_addition(self): 
-        a = ost.FourMomentum(px=100,py=1,pz=1,e=1)
-        b = ost.FourMomentum(px=50,py=1,pz=1,e=1)
+        a = ost.FourMomentum((100,1,1,1),'xyze')
+        b = ost.FourMomentum((50,1,1,1),'xyze')
         c = a +b 
 
         self.assertTrue(c.px(), a.px() + b.px() )
@@ -26,19 +22,18 @@ class TestFourMomentumMethods(unittest.TestCase):
         self.assertTrue(c.pz(), a.pz() + b.pz() )
         self.assertTrue(c.e(), a.e() + b.e() )
 
-    def test_p4_e_calc(self):
-        pass
-
     def test_p4_eta_calc(self):
-        a = ost.FourMomentum(px=100,py=1,pz=1,e=1)
+        a = ost.FourMomentum((100,1,1,1),'xyze')
         eta = .5*np.log( (np.sqrt(100.0**2 +2.0) + 1.)/(np.sqrt(100.0**2 +2.0) - 1.) )
         self.assertTrue(a.eta(), eta)
     
-    def test_p4_theta_calc(self):
-        pass
-    
     def test_p4_pt_calc(self):
-        pass
+        a = ost.FourMomentum((100,0.0,1,1),'xyze')
+        self.assertTrue(a.pt(), 100.0 )
+        a = ost.FourMomentum((0.0,100.0,1,1),'xyze')
+        self.assertTrue(a.pt(), 100.0 )
+        a = ost.FourMomentum((5.0,5.0,1,1),'xyze')
+        self.assertTrue(a.pt(), 25.0 )
     
 
 
