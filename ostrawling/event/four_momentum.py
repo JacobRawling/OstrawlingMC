f633d4eff8409775c92e9003db614f390f144c39
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 22:31:27 +0000

    Added property accessors to four-momentum helper class.

--- ostrawling/event/four_momentum.py
+++ ostrawling/event/four_momentum.py
@@ -24,8 +24,8 @@ class FourMomentum:
             self._py = vec[1]
             self._pz = vec[2]
             e = vec[3]
-            mag = np.abs(self.p()**2-e**2)
-            self._m = mag * np.sign(self.p()**2-e**2)
+            mag = np.abs(self.p**2-e**2)
+            self._m = mag * np.sign(self.p**2-e**2)
         elif basis == 'pt,eta,phi,m':
             self._px = vec[0]*np.cos(vec[2])
             self._py = vec[0]*np.sin(vec[2])
@@ -36,7 +36,7 @@ class FourMomentum:
             self._py = vec[0]*np.sin(vec[2])
             self._pz = vec[0]*np.sinh(vec[1])
             e = vec[3]
-            self._m = mag * np.sign(self.p()**2-vec[3]**2)
+            self._m = mag * np.sign(self.p**2-vec[3]**2)
 
         elif basis == 'p,theta,phi,m':
             self._px = vec[0]*np.cos(vec[2])
@@ -49,40 +49,50 @@ class FourMomentum:
             self._py = vec[0]*np.sin(vec[1])*np.sin(vec[2])
             self._pz = vec[0]*np.cos(vec[1])
             e = vec[3]
-            self._m = mag * np.sign(self.p()**2-vec[3]**2)
+            self._m = mag * np.sign(self.p**2-vec[3]**2)
 
         else:
             raise ValueError(' Basis %s is not one of the supported options. It must be "xyzm", "xyze", "pTEtaPhiE", "pTEtaPhiE"'%(basis))
 
+    @property
     def eta(self):
-        return 0.5*np.log( (self.p()+self.pz())/(self.p()-self.pz())  )
+        return 0.5*np.log( (self.p+self.pz)/(self.p-self.pz)  )
 
+    @property
     def p(self):
-        return np.sqrt(self.px()**2 + self.py()**2 + self.pz()**2)
+        return np.sqrt(self.px**2 + self.py**2 + self.pz**2)
 
+    @property
     def e(self):
-        return np.sqrt(self.m()**2 + self.p()**2 )
+        return np.sqrt(self.m**2 + self.p**2 )
 
+    @property
     def pt(self):
-        return np.sqrt(self.px()**2 + self.py()**2)
+        return np.sqrt(self.px**2 + self.py**2)
 
+    @property
     def px(self):
         return self._px
 
+    @property
     def py(self):
         return self._py
         
+    @property
     def pz(self):
         return self._pz
-        
+
+    @property
     def m(self):
         return self._m
 
+    @property
     def theta(self):
-        return np.arccos(self.pz()/self.p())
+        return np.arccos(self.pz/self.p)
 
+    @property
     def phi(self):
-        return np.arctan(self.py()/self.px())
+        return np.arctan(self.py/self.px)
 
 
     def __add__(self, b):
@@ -90,10 +100,10 @@ class FourMomentum:
         Define the result of self + b, where b is a four momenta
         """
         res = FourMomentum(
-                (self.px() + b.px(),
-                self.py() + b.py(),
-                self.pz() + b.pz(),
-                self.e() + b.e()),
+                (self.px + b.px,
+                self.py + b.py,
+                self.pz + b.pz,
+                self.e + b.e),
                 'x,y,z,e'
             )
         return res
--- tests/run_all_unit_tests.py
+++ tests/run_all_unit_tests.py
@@ -18,23 +18,23 @@ class TestFourMomentumMethods(unittest.TestCase):
         b = ost.FourMomentum((50,1,1,1),'x,y,z,e')
         c = a +b 
 
-        self.assertTrue(c.px(), a.px() + b.px() )
-        self.assertTrue(c.py(), a.py() + b.py() )
-        self.assertTrue(c.pz(), a.pz() + b.pz() )
-        self.assertTrue(c.e(), a.e() + b.e() )
+        self.assertTrue(c.px, a.px + b.px )
+        self.assertTrue(c.py, a.py + b.py )
+        self.assertTrue(c.pz, a.pz + b.pz )
+        self.assertTrue(c.e, a.e + b.e)
 
     def test_p4_eta_calc(self):
         a = ost.FourMomentum((100,1,1,1),'x,y,z,e')
         eta = .5*np.log( (np.sqrt(100.0**2 +2.0) + 1.)/(np.sqrt(100.0**2 +2.0) - 1.) )
-        self.assertTrue(a.eta(), eta)
+        self.assertTrue(a.eta, eta)
     
     def test_p4_pt_calc(self):
         a = ost.FourMomentum((100,0.0,1,1),'x,y,z,e')
-        self.assertTrue(a.pt(), 100.0 )
+        self.assertTrue(a.pt, 100.0 )
         a = ost.FourMomentum((0.0,100.0,1,1),'x,y,z,e')
-        self.assertTrue(a.pt(), 100.0 )
+        self.assertTrue(a.pt, 100.0 )
         a = ost.FourMomentum((5.0,5.0,1,1),'x,y,z,e')
-        self.assertTrue(a.pt(), 25.0 )
+        self.assertTrue(a.pt, 25.0 )
     
 class TestGeneratorMethods(unittest.TestCase):
     def test_init(self):
