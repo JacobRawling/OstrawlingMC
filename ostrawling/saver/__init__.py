cec9cc34eb6cf428839f4c9fbcfeaf06ebaa22f8
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 20:55:17 +0000

    Ignoring default output folder out

--- ostrawling/saver/__init__.py
+++ ostrawling/saver/__init__.py
@@ -1,1 +1,8 @@
 from .saver import *
+from .csv_saver import *
+
+def create_saver(output_file, saver='csv'):
+    if saver == 'csv':
+        return CSVSaver(output_file)
+        
+    return Saver(output_file)
