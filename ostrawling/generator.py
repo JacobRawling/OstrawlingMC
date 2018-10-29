543a0b2abd200d68e03722f2c86401831a3ef784
Jacob Rawling <mrjacob3@gmail.com>
2018-10-29 21:00:11 +0000

    Added some explanatory comments to methods.

--- ostrawling/generator.py
+++ ostrawling/generator.py
@@ -9,23 +9,41 @@ import logging
 from tqdm import tqdm 
 
 class Generator:
-    def __init__(self, process, saver='csv', config=None):
-        self.process = process
-        if not isinstance(self.process, Process):
-            raise ValueError('process argument of a Generator must be of type Process')
+    def __init__(self, process, config, saver='csv'):
+        """
+        @brief Helper class that performs the integration and Monte Carlo modelling
+        of a generic process.  
 
-        self.saver = create_saver(saver)
-        self.config = config
+        @params
+        process: A ostrawling.Process class that has a well defined implementation
+                 of an arbirtary process
+        saver: a string defining the saving output type
+        config: [NOT IMPLEMENTED YET] 
+
+        """
 
         logging.basicConfig(format='[OMC|%(levelname)s] %(message)s', level=logging.DEBUG)
         logging.info('Welcome to OstrawlingMC - a simple educational Monte Carlo')
 
+        self.process = process
+        if not isinstance(self.process, Process):
+            raise ValueError('process argument of a Generator must be of type Process')
+
+        self.saver = create_saver(config['output_file'], saver)
+        self.config = config
+
+
     def generate_n_event(self, n):
         """
         @Brief generate n events of process that is set at initalization
-
-
+        
+        @params
+        n: integer number of events
         """
+        if not isinstance(n, int):
+            raise ValueError('Can only generate an integer number of events and parameter n was not an integer!')
+
+
         logging.info('Generating %d events.'%n)
 
         events, total_integral = [], 0.
@@ -49,9 +67,11 @@ class Generator:
         return self.xsec
 
     def save(self):
-        pass
+        """
+        @Brief calls the open event saver and feeds it one event at a time to save
+        """
+        for e in self.events:   
+            self.saver.save_event(e)
 
-        # print(self.events)
-        # for e in self.events:   
-        #     self.saver.new_event(e)
+
 
--- runOMC.py
+++ runOMC.py
@@ -13,6 +13,7 @@ event_info = {
     's': 2000.0,
     'sqrt_s': np.sqrt(2000.0)
 }
+config = {'output_file': 'out/test.csv'}
 generator = ost.Generator(process=ost.processes.Dummy(event_info=event_info),
                       saver='csv',
                       config=None,
