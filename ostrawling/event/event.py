fce96d345ca91289cb8f040bbbad59709d265b3a
Kiran Ostrolenk <kirano@localhost.localdomain>
2018-11-01 16:18:37 +0000

    Failed attempt at adding integrand to ee_to_mumu.py.

--- ostrawling/event/event.py
+++ ostrawling/event/event.py
@@ -1,10 +1,10 @@
 
 class Event:
-    def __init__(self, sqrt_s, final_state_particles):
+    def __init__(self, sqrt_s, final_state_particles, initial_state_particles = []):
         self.final_state_particles = final_state_particles
+        self.initial_state_particles = initial_state_particles
         self.sqrt_s = sqrt_s
         self.weight = 1.0
 
     def set_weight(self, weight=1.0):
         self.weight = weight
-
